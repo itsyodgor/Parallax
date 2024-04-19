e5550e4397496703cf64d0eb18122d831db42ca4
Yodgorjon <vyodgorjon@gmail.com>
2024-04-19 18:23:08 +0500

    parallax tugadi

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const clouds = [...document.querySelectorAll('.header-cloud')]
 const boat = document.querySelector('.header-boat')
 
-window.addEventListener('scroll',() => {
+window.addEventListener('scroll', () => {
 
     clouds.forEach(cloud => {
 
@@ -20,13 +20,59 @@ const text = title.innerHTML
 console.log(text)
 title.innerHTML = null
 
-function str (x = 0) {
+function str(x = 0) {
     title.innerHTML += text[x]
     x++
-    if(x < text.length) {
+    if (x < text.length) {
         setTimeout(() => {
             str(x)
         }, 200);
     }
 }
 str()
+
+// parallax
+
+const parallaxBox = document.querySelector('.parallax-box')
+const parallaxBall = [...document.querySelectorAll('.parallax-ball')]
+
+parallaxBox.addEventListener("mousemove", (event) => {
+    parallaxBall.forEach(balls => {
+        const speed = balls.getAttribute('data-speed')
+        const x = ((window.innerWidth - event.pageX) * speed) / 10
+        const y = ((window.innerHeight - event.pageY) * speed) / 10
+
+        balls.style.transform = `translate(${x}px,${y}px)`
+    });
+})
+
+// Timer 
+
+const timerNum = [...document.querySelectorAll('.timer-num')]
+const timer = document.querySelector('.timer')
+
+window.addEventListener('scroll',function scrollTimer(){
+
+    if(this.scrollY >= timer.offsetTop - timer.offsetHeight){
+        timer.style.background = 'red'
+        timerStop()
+        this.removeEventListener('scroll',scrollTimer)
+    }
+})
+
+function timerStop () {
+    for(let i = 0; i < timerNum.length; i++){
+        let count = timerNum[i].getAttribute('data-num')
+
+        function calcCount (k = 0) {
+            timerNum[i].innerHTML = k
+            k++
+            if(k <= count){
+                setTimeout(() =>{
+                    calcCount(k)
+                }, 5);
+            }
+        }
+        calcCount()
+    }
+}
